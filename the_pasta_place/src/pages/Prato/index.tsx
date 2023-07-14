04c80cdea5653fc7a19395c986e6ab4ae0f10a82
Nickolas ramos <nickrferreti6@gmail.com>
2023-07-13 21:19:58 -0300

    Criando rota para descrição dos pratos

---
+++ the_pasta_place/src/pages/Prato/index.tsx
@@ -0,0 +1,10 @@
+import style from './prato.module.scss'
+import { useParams } from 'react-router-dom'
+
+export default function Prato() {
+    return(
+        <div>
+            Prato
+        </div>
+    )
+}
--- the_pasta_place/src/routes/routes.js
+++ the_pasta_place/src/routes/routes.js
@@ -2,12 +2,14 @@ import Footer from 'components/footer'
 import Menu from 'components/menu'
 import PagPadrao from 'components/pagPadrao'
 import Cardapio from 'pages/Cardapio'
+import Prato from 'pages/Prato'
 import Sobre from 'pages/Sobre'
 import Inicio from 'pages/inicio'
 import NotFound from 'pages/notFound'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default function AppRouter() {
+    let pratos = [...Cardapio]
     return (
         <main className='container'>
             <Router>
@@ -26,12 +28,16 @@ export default function AppRouter() {
                         />
                         <Route
                             path='sobre'
-                            element={<Sobre/>}
+                            element={<Sobre />}
+                        />
+                        <Route
+                            path='pratos/:id'
+                            element={<Prato />}
                         />
                     </Route>
                     <Route
                             path='*'
-                            element={<NotFound/>}
+                            element={<NotFound />}
                         />
                 </Routes>
                 <Footer/>
