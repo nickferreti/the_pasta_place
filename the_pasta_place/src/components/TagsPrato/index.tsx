0ace0021776d6af759e9fb442ad962ba28362e12
Nickolas ramos <nickrferreti6@gmail.com>
2023-07-14 18:15:25 -0300

    Resolvendo big do Header

--- the_pasta_place/src/components/TagsPrato/index.tsx
+++ the_pasta_place/src/components/TagsPrato/index.tsx
@@ -16,7 +16,7 @@ export default function TagsPrato({
       })}>{category.label}</div>
       <div className={styles.tags__porcao}>{size}g</div>
       <div className={styles.tags__qtdpessoas}> Serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
-      <div className={styles.tags__valor}>R$ {price.toFixed(2)}</div
+      <div className={styles.tags__valor}>R$ {price.toFixed(2)}</div>
     </div>
   );
 }
