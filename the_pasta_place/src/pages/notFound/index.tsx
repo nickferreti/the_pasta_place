7086287f9e9ade104684770d1fa8cea78c4695e6
Nickolas ramos <nickrferreti6@gmail.com>
2023-07-13 21:07:31 -0300

    Criando botão de voltar na pagina notFound

--- the_pasta_place/src/pages/notFound/index.tsx
+++ the_pasta_place/src/pages/notFound/index.tsx
@@ -2,13 +2,20 @@ import style from './notFound.module.scss'
 import {ReactComponent as NotFoundImage} from 'assets/not_found.svg'
 import classNames from 'classnames'
 import stylesTema from 'styles/tema.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 export default function NotFound() {
+    const navigate = useNavigate()
     return(
         <div className={classNames({
             [style.container]: true,
             [stylesTema.container]: true
         })}>
+            <div className={style.voltar}>
+                <button onClick={() => navigate(-1)}>
+                    {'< Voltar'}
+                </button>
+            </div>
             <NotFoundImage/>
         </div>
     )
